f517ce2c53ea93df65d4193cff7db17c9c3df78e
Moshe Immerman <moshe@flanksource.com>
2025-10-28 10:07:47 +0200

    chore: add --version

--- cmd/deps/main.go
+++ cmd/deps/main.go
@@ -6,7 +6,15 @@ import (
 	"github.com/flanksource/deps/cmd"
 )
 
+var (
+	version = "dev"
+	commit  = "unknown"
+	date    = "unknown"
+	dirty   = "false"
+)
+
 func main() {
+	cmd.SetVersion(version, commit, date, dirty)
 	if err := cmd.Execute(); err != nil {
 		os.Exit(1)
 	}
